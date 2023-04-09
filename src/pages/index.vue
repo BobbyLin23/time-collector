e383f7df4b104a0c53baa54ea4b39f95cc106b8f
FrankLin23 <linzhangsheng23@163.com>
2023-04-09 23:01:12 +0800

    feat: add home page

---
+++ src/pages/index.vue
@@ -0,0 +1,9 @@
+<script setup lang='ts'></script>
+
+<template>
+  <div w-full h-full flex flex-col justify-center items-center>
+    <h1>Time Collector</h1>
+  </div>
+</template>
+
+<style scoped></style>
--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import UnoCSS from 'unocss/vite'
 import { VitePWA } from 'vite-plugin-pwa'
+import Pages from 'vite-plugin-pages'
 
 export default defineConfig({
-  plugins: [vue(), UnoCSS(), VitePWA({ registerType: 'autoUpdate' })],
+  plugins: [vue(), UnoCSS(), VitePWA({ registerType: 'autoUpdate' }), Pages()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
