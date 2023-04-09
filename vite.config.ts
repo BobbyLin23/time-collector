2f1ce7e9a0039a826511fa4ef1d8f1319b5f6b58
FrankLin23 <linzhangsheng23@163.com>
2023-04-09 22:11:19 +0800

    chore: add config and dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
+import * as path from 'node:path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import UnoCSS from 'unocss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), UnoCSS(), VitePWA({ registerType: 'autoUpdate' })],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 })
